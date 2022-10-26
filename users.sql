fa2507e45ea4d1ccc02227b7899c5a0f456127d4
Badrudeen2 <badrubadrudeen100@gmail.com>
2022-10-26 10:55:01 -0700

    my commit

---
+++ users.sql
@@ -0,0 +1,5 @@
+INSERT INTO `Students` (`id`, `full_names`, `country`, `email`, `gender`, `password`) VALUES
+(2, 'Akanbi Taiwo', 'Nigeria', 'taiwo@gmail.com', 'Male', 'Phenom'),
+(3, 'Taiwo Akanbi', 'Nigeria', 'akanbi@gmail.com', 'Male', 'venom'),
+(4, 'Chioma Davido', 'Nigeria', 'chioma@gmail.com', 'Female', 'chuchu'),
+(8, 'Akanbi Badrudeen', 'Canada', 'badru@gmail.com', 'Male', 'badru2');
